a2cf43411e169847eadcdf07c6bce8be7ac2cb62
kyoh-dev <contact@kyoh.run>
2021-12-10 15:55:40 +1100

    Minor adjustments

--- functions.py
+++ functions.py
@@ -1,28 +1,34 @@
-from random import random
+from random import random, choice
 
 DEAD: int = 0
 ALIVE: int = 1
 
 
-def dead_state(width: int, height: int) -> list[list[int]]:
+def dead_state(height: int, width: int) -> list[list[int]]:
     return [[DEAD for _ in range(width)] for _ in range(height)]
 
 
-def random_state(width: int, height: int) -> list[list[int]]:
+def random_state(height: int, width: int) -> list[list[int]]:
     return [
         [DEAD if random() >= 0.5 else ALIVE for _ in range(width)]
         for _ in range(height)
     ]
 
 
-def render(state: list[list[int]]):
+def render(state: list[list[int]]) -> None:
     render_map = {
         DEAD: ' ',
-        ALIVE: u"\u2588"
+        ALIVE: choice((u'\u259A', u'\u259E'))
     }
 
     for row in state:
-        if state.index(row) > 0:
-            print('\n')
         for cell in row:
             print(render_map[cell], sep=None, end='')
+        print('\n')
+
+
+def next_state(state: list[list[int]]) -> list[list[int]]:
+    for x in range(len(state)):
+        for y in range(len(state[0])):
+            # TODO: calculate live neighbours
+            ...
